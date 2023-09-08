b029a20dbb61a93bc7b8ed1c1c0afacfb39f7642
Pablo Silva <pmarcelosilvag@gmail.com>
2023-09-08 13:23:01 -0400

    add: ejemplos de encapsulamiento

--- POO/POOEncapsulamiento.py
+++ POO/POOEncapsulamiento.py
@@ -47,3 +47,36 @@ print(bisa.get_balance())
 bisa.wihtdraw(500)
 print(bisa.get_balance())
 bisa.wihtdraw(50)
+
+# GETTERS Y SETTERS
+
+# class User:
+#     def __init__(self,username=None) -> None:
+#         self.__username=username
+#     def getusername(self):
+#         return self.__username
+#     def setusername(self,username):
+#         self.__username=username
+
+# user1=User('Pablo')
+# print("antes de setter:",user1.getusername())
+# user1.setusername('Chanchito feliz')
+# print("Despues de getter:",user1.getusername())
+        
+#-------------OTRA FORMA DE HACERLO ES USARLO DECORADORES
+
+class User:
+    def __init__(self,username=None) -> None:
+        self.__username=username
+    @property   #este estaria actuando como el getter
+    def username(self):
+        return self.__username
+    
+    @username.setter
+    def username(self,username):
+        self.__username=username
+
+user2=User('Pablo')
+print("antes de setter:",user2.username)
+user2.username='Chanchito'
+print("Despues de setter:",user2.username)  
---
+++ POO/POOlab2.py
@@ -0,0 +1,13 @@
+
+class rectangle:
+    def __init__(self,length:float,width:float) -> None:
+        self.__length=length
+        self.__width=width
+    def area(self):
+        return self.__length*self.__width
+    def perimeter(self):
+        return (self.__length+self.__width)*2
+
+rect=rectangle(3.6,5.9)
+print("AREA",round(rect.area(),2))
+print("PERIMETER",round(rect.perimeter(),2))
