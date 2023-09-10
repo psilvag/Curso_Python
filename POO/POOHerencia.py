552c885a60af040e598e555153f571fa7db53fe4
Pablo Silva <pmarcelosilvag@gmail.com>
2023-09-09 23:36:56 -0400

    add: Funcion herencia

---
+++ POO/POOHerencia.py
@@ -0,0 +1,23 @@
+ 
+class Vehicle:
+    def __init__(self,make,color,model):
+        self.make=make
+        self.color=color
+        self.model=model
+    def show_car(self):
+        print("MAKE:",self.make)
+        print("COLOR:",self.color)
+        print("MODEL:",self.model)
+
+class car(Vehicle):
+    def __init__(self, make, color, model,numdoors):
+        super().__init__(make, color, model)
+        self.numdoors=numdoors
+    
+    def printDetails(self):
+        self.show_car()
+        print("NUM DOORS:",self.numdoors)
+
+tesla=car('Tesla','red',2019,5)
+tesla.printDetails()
+tesla.show_car()
